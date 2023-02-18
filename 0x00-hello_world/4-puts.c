ea8806738125f18119bcb417a6310f599e415009
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-02-18 13:51:53 +0000

    Made the source for Hello World Task 4 Betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Uses puts to print a string
+ *
+ * Return: 0 at the end
+ */
 int main(void)
 {
+	/* declarations */
+
+	/* statements  */
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
