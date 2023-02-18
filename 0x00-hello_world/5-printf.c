e9e687089cedbf91b15f86a919a4b10f408f9b63
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-02-18 14:03:53 +0000

    Made the source for Hello World Task 5 Betty compliant

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - prints a string using printf
+ *
+ * Description: Uses printf to print
+ * "with proper grammar, but the outcome is a piece of art
+ * followed by a new line
+ * Return: O to signal execution went through
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return(0);
+	return (0);
 }
