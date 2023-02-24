00db9d26eee10b838b9d766a7949e17b3f25bcd2
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-02-24 16:37:32 +0000

    Corrected source for Variables, if, else, while Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int c;
 
-	for (c = 48; c < 57; c++)
+	for (c = 48; c < 58; c++)
 	{
 		putchar(c);
-		putchar(44);
-		putchar(32);
+		if (c < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	putchar(57);
+	putchar(10);
 
 	return (0);
 }
