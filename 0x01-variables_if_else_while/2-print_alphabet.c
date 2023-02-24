17783c1d391aa32659288378ea3664dcaecca159
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-02-24 15:31:29 +0000

    Added source code for Variables, if, else, while Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower case followed by a new line
+ * using putchar
+ *
+ * Return: 0 to signal good execution
+ */
+int main(void)
+{
+	int c;
+	for (c = 97, c < 123, c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
