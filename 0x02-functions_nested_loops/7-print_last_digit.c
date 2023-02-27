893b460566511241e0c66c11db0dadb04f41a1d3
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-02-27 15:15:09 +0000

    Added source for Task 7 in Functions, nested loops project

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number whose last digit is printed
+ *
+ * Return: the last digit of n.
+ */
+int print_last_digit(int n)
+{
+	/* Declarations */
+	int d;
+
+	/* Statements */
+	d = n % 10;
+	_putchar('0' + d);
+	return (d);
+}
