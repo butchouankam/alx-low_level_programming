8a44294509bc4d01af57b8488779901e1a11b1a1
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-02-27 09:49:56 +0000

    Added README, Task O source and main header for Functions, nested loops project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -  prints "_putchar" followed by a new line
+ *
+ * Return: 0 for good execution
+ */
+int main(void)
+{
+	/* Declarations */
+
+	/* Statements */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
