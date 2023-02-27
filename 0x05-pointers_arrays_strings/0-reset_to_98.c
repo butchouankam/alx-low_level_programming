b08ece2e1907304780281a7ce31cc45c5a9e1d32
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-02-27 19:58:52 +0000

    Added source for Task 0 of Pointers, arrays, and strings project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and update
+ * the value its points to to 98.
+ * @n: the pointer to the value to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
