f157e7adcb434d7ca0bba77a65b589c8ffc75ca4
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-03-05 10:13:14 +0000

    Added Task O source for Pointers, arrays and strings II

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+void _putchar(char *c);
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ *
+ * Description: This function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest, and then
+ * adds the terminating null byte.
+ * Return: a pointer to the string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	/* Declarations */
+	int idst, iscr; /* indexes */
+
+	/* Statements */
+	idst = 0;
+	isrc = 0;
+	while (*(src + isrc) != '\0')
+	{
+		if (*(dest + idst) == '\0')
+		{
+			*(dest + idst) = *(src + isrc);
+			*(dest + idst + 1) = '\0';
+			isrc++;
+		}
+		idst++;
+	}
+	return (dest);
+}
