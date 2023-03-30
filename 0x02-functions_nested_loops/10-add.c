b636fbe16a71ec8e9fc538f18b48480bbea9c1a2
Brice Ulrich Tchouankam <butchouankam@gmail.com>
2023-03-30 18:25:17 +0000

    Added source file for Task 10 of C-Functions, nested loops project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two integers and returns the result
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
